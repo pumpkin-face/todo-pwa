f5fcefb425ce6f22dc44f21812c9def5d890607c
pumpkin-face <2122100598@soy.utj.edu.mx>
2025-09-05 17:56:59 -0600

    Step One(V2): Configuration Server to MongoDB & Organizations Folders

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-import { Express } from "express";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// import taskRoutes from "./routes/taskRoutes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Paths
+// app.use('/api/tasks', taskRoutes);
+
+mongoose.connect(process.env.MONGODB_URI!)
+.then(() => {
+    console.log("Connection Succesful to MongoDB");
+    app.listen(PORT, () => console.log('Server eunning on port ${PORT}'));
+})
+.catch((err) => console.error('Error connecting to MongoDB:', err));
+
